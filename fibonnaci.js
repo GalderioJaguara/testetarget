ce3af43950b68c6fa837380687c6fa90b29048e3
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2024-09-23 18:51:59 -0300

    Update fibonnaci.js

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,7 +1,27 @@
 function fibonacci(n) {
-    let a = 1, b = 1;
-    for (let i = 2; i < n; i++) {
-        [a, b] = [b, a + b];
+    const fibonacci = [];
+    fibonacci[1] = 1;
+    fibonacci[2] = 1;
+    
+    for(let i = 3; i < n; i++){
+       fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
     }
-    return b;
+    for (let i = 1; fibonacci.length > i; i++){
+        console.log(fibonacci[i]);
+    }
+    if (fibonacci.includes(n)){
+        console.log("este número pertence a sequência!");
+    } else {
+        console.log("este número não pertence a sequência!");
+    }
+
+    return fibonacci;
 }
+
+function main() {
+    let iniciador = 4;
+    fibonacci(iniciador);
+    
+}
+
+main();
